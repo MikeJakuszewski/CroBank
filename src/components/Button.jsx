4c0bbd652fb430afc256a40bb813eb8f487863d6
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-08-30 13:13:09 -0400

    completed business section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,11 @@
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles, setText }) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
+    >
+      {setText}
+    </button>
+  );
 };
 export default Button;
